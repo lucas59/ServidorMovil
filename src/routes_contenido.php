eeff5296e0b2a0ed7c708002298c2f60df061b0d
isma4102 <santoscastelvi2014@gmail.com>
2019-05-25 20:56:03 -0300

    Comentarios

--- src/routes_contenido.php
+++ src/routes_contenido.php
@@ -16,18 +16,16 @@ return function (App $app){
 	});
 
 
-$app->post('/contenido/comentario',function($request,$response,$args) use ($container){
-		$data = $request->getParams();
-		$texto=$data['comentario'];
-		$titulo=$data['titulo'];
-		$capitulo_id=$data['capitulo_id'];
-		$contenido_id=$data['contenido_id'];
-		$usuario=$data['usuario'];
-		$fecha=$data['fecha'];
-		$genero=$data['genero'];
-		$titulo_elemento=$data['titulo_elemento'];
+	$app->get('/contenido/comentario',function($request,$response,$args) use ($container){
+		$texto=$request->getQueryParam("comentario");
+		$capitulo_id=$request->getQueryParam("capitulo_id");
+		$contenido_id=$request->getQueryParam("contenido_id");
+		$usuario=$request->getQueryParam("usuario");
+		$fecha=$request->getQueryParam("fecha");
+		$genero=$request->getQueryParam("genero");
+		$titulo_elemento=$request->getQueryParam("titulo_elemento");
 		$myObj = new \stdClass();
-		$validacion = ctr_contenido::Comentario($texto,$titulo,$capitulo_id,$contenido_id,$usuario,$fecha,$genero,$titulo_elemento);
+		$validacion = ctr_contenido::Comentario($texto,$capitulo_id,$contenido_id,$usuario,$fecha,$genero,$titulo_elemento);
 		if($validacion == "1"){
 			$myObj->retorno = true; 
 		}else{
@@ -36,6 +34,12 @@ $app->post('/contenido/comentario',function($request,$response,$args) use ($cont
 		return json_encode($myObj);
 
 	})->setName("Comentario");
+
+	$app->get('/contenido/lista_comentario',function($request,$response,$args) use ($container){
+		$id=$request->getQueryParam("id");
+		$validacion = ctr_contenido::Lista_Comentario($id);
+		return $validacion;
+	})->setName("Lista_comentario");
 
 
 
